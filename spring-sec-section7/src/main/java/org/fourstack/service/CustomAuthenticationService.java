6728ebb8db908298c59f3a6381f6a699cf04fe19
Manjunath HM <manjunath@altimetrik.com>
2025-03-03 13:05:06 +0530

    A service file to Load the Security Context to get Authentication information.

---
+++ spring-sec-section7/src/main/java/org/fourstack/service/CustomAuthenticationService.java
@@ -0,0 +1,21 @@
+package org.fourstack.service;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomAuthenticationService {
+
+  public String getUserName() {
+    SecurityContext context = SecurityContextHolder.getContext();
+    if (context != null) {
+      Authentication authentication = context.getAuthentication();
+      if (authentication != null) {
+        return authentication.getName();
+      }
+    }
+    return null;
+  }
+}
